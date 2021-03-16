564eeac9dca81d9cdd1fd3c69f93f310a3eb9bb8
kotashovv <78229202+kotashovv@users.noreply.github.com>
2021-03-16 23:54:09 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -8,9 +8,21 @@ $(document).ready(function(){
         $('body').removeClass('lock');
     })
     $('.slider').slick({
-        dots: true,
+        dots: false,
         arrows: false,
         autoplay: true,
     });
+    const anchors = document.querySelectorAll('a[href="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event) {
+            event.preventDefault();
+            const BlockID = anchor.getAttribute('href')
+            document.querySelector('' + BlockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        });
+    };
 });
 
